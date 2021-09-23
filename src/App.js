952fa6b247d47ab324b486c1601bc8dd16c33352
nabilhayet <nabil4457@yahoo.com>
2021-09-23 17:38:12 -0400

    fetch all items and grouped them by listId

--- src/App.js
+++ src/App.js
@@ -1,11 +1,59 @@
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
 
-    </div>
-  );
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [],
+      name: ""
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://fetch-hiring.s3.amazonaws.com/hiring.json")
+      .then(response => response.json())
+      .then(items => {
+        items.forEach(element => {
+          if (element.name != "" && element.name != null) {
+            this.setState({
+              items: [...this.state.items, element]
+            })
+          }
+        });
+      })
+  }
+
+  render() {
+    const getItems = this.state.items.reduce((itemsSoFar, { listId, name }) => {
+      if (!itemsSoFar[listId]) itemsSoFar[listId] = [];
+      itemsSoFar[listId].push(name);
+      return itemsSoFar;
+    }, {});
+
+    const keys = Object.keys(getItems)
+    const values = keys.map(k => {
+      return (
+        getItems[k].map((v) => {
+          return (
+            <div>
+              {k} ----- {v}
+            </div>
+          )
+        })
+      )
+    })
+    return (
+      <div className="App" >
+        <h1>List Items</h1>
+        <button onClick={this.showItems}>Sort By ListId</button>
+        <button onClick={this.showItems}>Sort By Name</button>
+        {values}
+
+      </div>
+    );
+  }
 }
 
 export default App;
