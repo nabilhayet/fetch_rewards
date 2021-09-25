d8b85cc45da25f3b421dceb83787cdbc8fec4b70
nabilhayet <nabil4457@yahoo.com>
2021-09-25 13:53:39 -0400

    added styling for list item

--- src/Item.js
+++ src/Item.js
@@ -2,19 +2,17 @@ import { Component } from 'react';
 import './App.css';
 
 class Item extends Component {
-
     constructor(props) {
         super(props)
         this.state = {
-            items: []
+            items: [],
+            disabled: false
         }
     }
-
     getItems = () => {
         const myObj = {}
         this.props.list.map((item) => {
             let getListId = item.listId
-
             if (!myObj[getListId]) {
                 myObj[getListId] = []
             } else {
@@ -23,8 +21,67 @@ class Item extends Component {
             return item
         })
         this.setState({
-            items: [...this.state.items, myObj]
+            items: [...this.state.items, myObj],
+            disabled: true
         })
+
+
+    }
+
+    renderTableHeader() {
+        const getHeader = this.state.items.map(obj => {
+            const header = Object.entries(obj)[0]
+            return header.map((key) => {
+                if (key.length == 1) {
+                    return (
+                        <th id="1">List Id</th>
+                    )
+                } else {
+                    return (
+                        <th id="80">List Name</th>
+                    )
+
+                }
+            })
+        })
+        return getHeader
+    }
+
+    renderTableData() {
+        const object = this.state.items.map(obj => {
+            const keyValuePair = Object.entries(obj)
+            const values = keyValuePair.map((entries) => {
+                const v = entries.map((e) => {
+                    let newArray = [];
+                    if (e.length > 1) {
+                        const a = e.map((arr) => {
+                            const splitArray = arr.split(" ")
+                            newArray.push(splitArray[1])
+                        })
+                        newArray = newArray.map(Number).sort(this.compare)
+                        const s = newArray.map((b) => {
+                            return (
+                                <tr>
+                                    <td><li>{`Item ${b}`}</li></td>
+                                </tr>
+
+                            )
+                        })
+                        return s;
+                    }
+                    else {
+                        return (
+                            <tr>{e}</tr>
+
+
+                        )
+                    }
+                })
+                return v;
+            })
+            return values
+        })
+        return object
     }
 
     compare = (a, b) => {
@@ -41,41 +98,18 @@ class Item extends Component {
     }
 
     render() {
-        const object = this.state.items.map(obj => {
-            const keyValuePair = Object.entries(obj)
-            const values = keyValuePair.map((entries) => {
-                const v = entries.map((e) => {
-                    let newArray = [];
-                    if (e.length > 1) {
-                        const a = e.map((arr) => {
-                            const splitArray = arr.split(" ")
-                            newArray.push(splitArray[1])
-                        })
-                        newArray = newArray.map(Number).sort(this.compare)
-                        const s = newArray.map((b) => {
-                            return (
-                                <li>{`Item ${b}`}</li>
-                            )
-                        })
-                        return s;
-
-                    }
-                    else {
-                        return (
-                            <li>{e}</li>
-                        )
-                    }
-                })
-                return v;
-            })
-            return values
-        })
-
-
         return (
             <div>
-                <button onClick={this.getItems}>VIEW Items</button>
-                {object}
+                <button onClick={this.getItems} disabled={this.state.disabled}>VIEW Items</button>
+                <table id='students'>
+                    <thead>
+                        <tr>{this.renderTableHeader()}</tr>
+                    </thead>
+                    <tbody>
+                        {this.renderTableData()}
+                    </tbody>
+                </table>
+
             </div >
         )
     }
